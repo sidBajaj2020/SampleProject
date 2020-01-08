8b97a3e3af374f6277ea114981fd5df48962a519
sidBajaj2020 <sidharth.bajaj@MLA0RLI009.local>
2020-01-08 11:25:17 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
 import React from 'react';
-import logo from './logo.svg';
+import Main from './Main'; 
+import { fade, makeStyles } from '@material-ui/core/styles';
+import { Typography, Button } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import InputBase from '@material-ui/core/InputBase';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import Link from '@material-ui/core/Link';
 import './App.css';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }, 
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
+}));
+
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <AppBar color="primary" position="static">
+          <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Grid container direction="row" justify="center">
+            <Grid item>
+            <Typography variant="h6" className={classes.title}>
+              <i>Play Date</i>
+            </Typography>
+            </Grid>
+          </Grid>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <Link href="#" color="inherit">
+            <Button color="inherit">Login</Button>
+          </Link>
+          </Toolbar>
+        </AppBar>
+        <Main/>
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import styled from 'styled-components'
+
+const Header1 = styled.h1`
+    padding-top: 5em; 
+    margin: 0.5em 4em; 
+    font-size: 3em
+    color: #800000
+`
+
+const Header2 = styled.h1`
+    padding-right: 2.0em;
+    margin: 0.5em 6.5em; 
+    font-size: 2em
+`
+
+export default function Main(){
+    return (
+        <Grid container>
+            <Grid item xs="8">
+                <Header1>Life is Short.</Header1>
+                <Header2>Arrange a Play Date.</Header2>
+            </Grid>
+                <Grid item xs="4" container direction="column-reverse">
+                    <Grid item>
+                        <img src="https://upload.wikimedia.org/wikipedia/en/d/d4/Mickey_Mouse.png"></img>
+                    </Grid>
+                </Grid>
+        </Grid>
+    )
+}
